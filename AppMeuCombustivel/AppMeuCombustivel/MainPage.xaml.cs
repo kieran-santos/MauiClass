a40108cebf9598275d3c3200c9ee67a8e3df3ad9
Kieran Santos <104686090+kieran-santos@users.noreply.github.com>
2026-03-30 19:12:42 -0300

    Add files via upload

---
+++ AppMeuCombustivel/AppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace AppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {   //Método que define o que a interface deve fazer
+            //É possível identificar qual botão disparou o evento, pois vários podem chamar o mesmo evento (definido pelo object sender)
+
+            double preco_gasolina = Convert.ToDouble(gasolina.Text);
+            double preco_etanol = Convert.ToDouble(etanol.Text);
+            string msg = "";
+
+            if(preco_etanol > (preco_gasolina * 0.7))
+            {
+                msg = "Compensa a gasolina";
+            } else
+            {
+                msg = "Compensa o etanol";
+            }
+            await DisplayAlertAsync("Resultado:", msg, "OK");
+        }
+    }
+}
